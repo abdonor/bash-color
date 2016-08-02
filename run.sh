f421e15a33cf6fb0b8576e5a109912fe6ff16bf3
@bd0n0r <albert.abdonor@gmail.com>
2016-08-02 19:28:10 -0300

    bugfix

--- run.sh
+++ run.sh
@@ -2,18 +2,23 @@ cd /home
 mkdir abdonor
 cd ./abdonor
 name=`cat /etc/hostname`
-echo $name > nameMachine.txt
+echo '('$name')' > nameMachine.txt
 
-echo -e "export PS1=\"\${NAMEFILE} \${debian_chroot:+(\$debian_chroot)}\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ \" \n$(cat ~/.bashrc)" > ~/.bashrc
-echo -e "NAMEFILE=`cat /home/abdonor/nameMachine.txt` \n$(cat ~/.bashrc)" > ~/.bashrc
-echo -e "source \"${GIT_ABDONOR_PROMPT}/main.sh\" \n$(cat ~/.bashrc)" > ~/.bashrc
-echo -e "export GIT_ABDONOR_PROMPT=~/.bash/bash-color\n$(cat ~/.bashrc)" > ~/.bashrc
+CMM1='export GIT_ABDONOR_PROMPT=~/.bash/bash-color'
+CMM2='source "${GIT_ABDONOR_PROMPT}/main.sh"'
+CMM3='NAMEFILE=`cat /home/abdonor/nameMachine.txt`'
+CMM4='export PS1="\${NAMEFILE} \${debian_chroot:+(\$debian_chroot)}\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ " '
 
-echo "export GIT_ABDONOR_PROMPT=~/.bash/bash-color" >> ~/.bashrc
-echo "source \"${GIT_ABDONOR_PROMPT}/main.sh\" " >> ~/.bashrc
-echo "NAMEFILE=`cat /home/abdonor/nameMachine.txt`" >> ~/.bashrc
-echo "export PS1=\"\${NAMEFILE} \${debian_chroot:+(\$debian_chroot)}\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ \" " >> ~/.bashrc
+echo -e $CMM4"\n$(cat ~/.bashrc)" > ~/.bashrc
+echo -e $CMM3"\n$(cat ~/.bashrc)" > ~/.bashrc
+echo -e $CMM2"\n$(cat ~/.bashrc)" > ~/.bashrc
+echo -e $CMM1"\n$(cat ~/.bashrc)" > ~/.bashrc
+
+echo $CMM1 >> ~/.bashrc
+echo $CMM2 >> ~/.bashrc
+echo $CMM3 >> ~/.bashrc
+echo $CMM4  >> ~/.bashrc
 
 source ~/.bashrc
 
-echo "/n ABDONOR's COLOR BASH CONFIG IS DONE, RESTART YOUR PC!"
+echo "ABDONOR's COLOR BASH CONFIG IS DONE, RESTART YOUR PC!"

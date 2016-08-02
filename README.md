2c5cbcd0c396686b7a68f54c10b32b4f1087056f
@bd0n0r <albert.abdonor@gmail.com>
2016-08-02 18:45:49 -0300

    bugfix

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Run the commands:
 mkdir ~/.bash
 cd ~/.bash
 git clone https://github.com/abdonor/bash-color.git
+chmod 777 -R ~/.bash/
 ~/.bash/bash-color/run.sh
 ```
 
--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@ cd ./abdonor
 name=`cat /etc/hostname`
 echo $name > nameMachine.txt
 
+echo -e "export PS1=\"\${NAMEFILE} \${debian_chroot:+(\$debian_chroot)}\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ \" \n$(cat ~/.bashrc)" > ~/.bashrc
+echo -e "NAMEFILE=`cat /home/abdonor/nameMachine.txt` \n$(cat ~/.bashrc)" > ~/.bashrc
+echo -e "source \"${GIT_ABDONOR_PROMPT}/main.sh\" \n$(cat ~/.bashrc)" > ~/.bashrc
+echo -e "export GIT_ABDONOR_PROMPT=~/.bash/bash-color\n$(cat ~/.bashrc)" > ~/.bashrc
+
 echo "export GIT_ABDONOR_PROMPT=~/.bash/bash-color" >> ~/.bashrc
 echo "source \"${GIT_ABDONOR_PROMPT}/main.sh\" " >> ~/.bashrc
 echo "NAMEFILE=`cat /home/abdonor/nameMachine.txt`" >> ~/.bashrc
@@ -11,4 +16,4 @@ echo "export PS1=\"\${NAMEFILE} \${debian_chroot:+(\$debian_chroot)}\[\033[01;33
 
 source ~/.bashrc
 
-echo "\n ABDONOR's COLOR BASH CONFIG IS DONE, RESTART YOUR PC!"
+echo "/n ABDONOR's COLOR BASH CONFIG IS DONE, RESTART YOUR PC!"
